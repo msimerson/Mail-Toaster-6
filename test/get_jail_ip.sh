7d45d39982d392a5b66050c74d99a215bcab70b2
Matt Simerson <matt@tnpi.net>
2016-11-27 21:32:53 -0800

    try an || for travis env

--- test/get_jail_ip.sh
+++ test/get_jail_ip.sh
@@ -1,13 +1,17 @@
-. mail-toaster.sh
+. mail-toaster.sh || . ../mail-toaster.sh
 
 IP=$(get_jail_ip mysql)
-if [ "$IP" != "172.16.15.4" ]; then
+if [ "$IP" = "172.16.15.4" ]; then
+    echo "mysql IP is $IP"
+else
     echo "ERR: mysql IP is not $IP"
     exit 2
 fi
 
 IP=$(get_jail_ip haraka)
-if [ "$IP" != "172.16.15.9" ]; then
+if [ "$IP" = "172.16.15.9" ]; then
+    echo "haraka IP is $IP"
+else
     echo "ERR: haraka IP is not $IP"
     exit 2
 fi
