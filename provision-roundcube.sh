cfbef818085749341a845d3e3069887f61c15193
Matt Simerson <matt@tnpi.net>
2016-12-08 14:09:37 -0800

    sync php path with haproxy url shorten

--- provision-roundcube.sh
+++ provision-roundcube.sh
@@ -184,10 +184,10 @@ install_nginx()
 -        #    include        fastcgi_params;
 -        #}
 +        location ~ \.php$ {
-+            root           /usr/local/www;
++            root           /usr/local/www/roundcube;
 +            fastcgi_pass   127.0.0.1:9000;
 +            fastcgi_index  index.php;
-+            fastcgi_param  SCRIPT_FILENAME  $document_root/$fastcgi_script_name;
++            fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
 +            include        fastcgi_params;
 +        }
  
