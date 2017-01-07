f1edee851ee6fb868d51b4c32e3de6ae3c55a401
Matt Simerson <matt@tnpi.net>
2017-01-06 23:22:26 -0800

    smf script (#170)

---
+++ provision-smf.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+
+# shellcheck disable=1091
+. mail-toaster.sh || exit
+
+export JAIL_START_EXTRA=""
+export JAIL_CONF_EXTRA=""
+
+mt6-include php
+mt6-include nginx
+
+install_smf()
+{
+	install_nginx
+	install_php 56 "mysql session gd"
+
+	if [ ! -d "$STAGE_MNT/usr/local/www/smf" ]; then
+		mkdir -p "$STAGE_MNT/usr/local/www/smf" || exit
+	fi
+
+	fetch -m -o "$STAGE_MNT/data/smf.tar.bz2" \
+		http://download.simplemachines.org/index.php/smf_2-0-13_install.tar.bz2
+
+	stage_exec sh -c 'cd /usr/local/www/smf; bunzip2 -c /data/smf.tar.bz2 | tar -xf -' || exit
+	stage_exec sh -c 'cd /usr/local/www/smf; chown www:www attachments avatars cache Packages Packages/installed.list Smileys Themes agreement.txt Settings.php Settings_bak.php'
+
+	stage_pkg_install aspell
+}
+
+configure_nginx_server()
+{
+	tee "$STAGE_MNT/data/etc/nginx-server.conf" <<'EO_SMF_NGINX'
+
+	server {
+		listen       80;
+
+		set_real_ip_from haproxy;
+		real_ip_header X-Forwarded-For;
+		client_max_body_size 25m;
+
+		location ~ ^/forum/(.+\.php)$ {
+			alias          /usr/local/www/smf;
+			fastcgi_pass   127.0.0.1:9000;
+			fastcgi_index  index.php;
+			fastcgi_param  SCRIPT_FILENAME  $document_root/$1;
+			include        /usr/local/etc/nginx/fastcgi_params;
+		}
+
+		location /forum/ {
+			alias /usr/local/www/smf/;
+			index index.php;
+			try_files $uri $uri/ =404;
+		}
+
+		error_page   500 502 503 504  /50x.html;
+		location = /50x.html {
+			root   /usr/local/www/nginx-dist;
+		}
+	}
+EO_SMF_NGINX
+
+	sed -i .bak \
+		-e "s/haproxy/$(get_jail_ip haproxy)/" \
+		"$STAGE_MNT/data/etc/nginx-server.conf"
+}
+
+configure_smf()
+{
+	configure_php smf
+	configure_nginx smf
+	configure_nginx_server
+
+	if [ -f "ZFS_DATA_MNT/smf/Settings.php" ]; then
+		tell_status "installing localized Settings.php"
+		cp "ZFS_DATA_MNT/smf/Settings.php" "$STAGE_MNT/usr/local/www/smf/" || exit
+	else
+		tell_status "post-install configuration will be required"
+		sleep 2
+	fi
+
+	stage_sysrc nginx_flags='-c /data/etc/nginx.conf'
+}
+
+start_smf()
+{
+	start_php_fpm
+	start_nginx
+}
+
+test_smf()
+{
+	test_php_fpm
+	test_nginx
+	echo "it worked"
+}
+
+base_snapshot_exists || exit
+create_staged_fs smf
+start_staged_jail
+install_smf
+configure_smf
+start_smf
+test_smf
+promote_staged_jail smf
