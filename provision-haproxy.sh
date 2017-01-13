c2bf70d240e452748b8e6d74e96951d18134d938
Matt Simerson <matt@tnpi.net>
2017-01-12 20:07:15 -0800

    haproxy: always put conf on /data

--- provision-haproxy.sh
+++ provision-haproxy.sh
@@ -14,35 +14,42 @@ install_haproxy()
 
 configure_haproxy()
 {
-	if [ -f "$ZFS_DATA_MNT/haproxy/etc/haproxy.conf" ]; then
-		tell_status "using $ZFS_DATA_MNT/haproxy/etc/haproxy.conf"
-		stage_sysrc haproxy_config="/data/etc/haproxy.conf"
+	stage_sysrc haproxy_config="/data/etc/haproxy.conf"
+
+	if [ ! -d "$ZFS_DATA_MNT/haproxy/etc" ]; then
+		tell_status "creating /data/etc"
+		mkdir -p "$ZFS_DATA_MNT/haproxy/etc" || exit
+	fi
+
+	local _data_cf="$ZFS_DATA_MNT/haproxy/etc/haproxy.conf"
+	if [ -f "$_data_cf" ]; then
+		tell_status "preserving $_data_cf"
 		return
 	fi
 
 	tell_status "configuring MT6 default haproxy"
-	tee "$STAGE_MNT/usr/local/etc/haproxy.conf" <<EO_HAPROXY_CONF
+	tee "$_data_cf" <<EO_HAPROXY_CONF
 global
-    daemon
-    maxconn     256  # Total Max Connections. This is dependent on ulimit
-    nbproc      1
-    ssl-default-bind-options no-sslv3 no-tls-tickets
-    ssl-dh-param-file /data/ssl/dhparam.pem
-    tune.ssl.default-dh-param 2048
+	daemon
+	maxconn     256  # Total Max Connections. This is dependent on ulimit
+	nbproc      1
+	ssl-default-bind-options no-sslv3 no-tls-tickets
+	ssl-dh-param-file /data/ssl/dhparam.pem
+	tune.ssl.default-dh-param 2048
 
 defaults
-    mode        http
-    balance     roundrobin
-    option      forwardfor   # set X-Forwarded-For
-    option      httpclose
-    option      http-server-close
-    option      log-separate-errors
-    log         global
-    timeout     connect 5s
-    timeout     server 30s
-    timeout     client 30s
+	mode        http
+	balance     roundrobin
+	option      forwardfor   # set X-Forwarded-For
+	option      httpclose
+	option      http-server-close
+	option      log-separate-errors
+	log         global
+	timeout     connect 5s
+	timeout     server 30s
+	timeout     client 30s
 #   timeout     client 86400s
-    timeout     tunnel 1h
+	timeout     tunnel 1h
 
 #listen stats *:9000
 #    mode http
@@ -53,114 +60,119 @@ defaults
 #    stats admin if TRUE
 
 frontend http-in
-    bind *:80
-    acl is_websocket hdr(Upgrade) -i WebSocket
-    acl is_websocket hdr_beg(Host) -i ws
-    use_backend websocket_haraka    if  is_websocket
-    redirect scheme https code 301 if !is_websocket !{ ssl_fc }
+	bind *:80
+	bind *:443 ssl crt /etc/ssl/private
+	# ciphers AES128+EECDH:AES128+EDH
 
-frontend https-in
-    bind *:443 ssl crt /etc/ssl/private
-    # ciphers AES128+EECDH:AES128+EDH
-    reqadd X-Forwarded-Proto:\ https
-    default_backend www_webmail
+	http-request  set-header X-Forwarded-Proto https if { ssl_fc }
+	http-request  set-header X-Forwarded-Port %[dst_port]
+	http-response set-header X-Frame-Options sameorigin
 
-    acl is_websocket hdr(Upgrade) -i WebSocket
-    acl is_websocket hdr_beg(Host) -i ws
+	acl is_websocket hdr(Upgrade) -i WebSocket
+	acl is_websocket hdr_beg(Host) -i ws
+	redirect scheme https code 301 if !is_websocket !{ ssl_fc }
 
-    acl munin        path_beg /munin
-    acl nagios       path_beg /nagios
-    acl watch        path_beg /watch
-    acl haraka       path_beg /haraka
-    acl haraka       path_beg /logs
-    acl qmailadmin   path_beg /qmailadmin
-    acl qmailadmin   path_beg /cgi-bin/qmailadmin
-    acl sqwebmail    path_beg /sqwebmail
-    acl sqwebmail    path_beg /cgi-bin/sqwebmail
-    acl isoqlog      path_beg /isoqlog
-    acl rspamd       path_beg /rspamd
-    acl roundcube    path_beg /roundcube
-    acl rainloop     path_beg /rainloop
-    acl squirrelmail path_beg /squirrelmail
-    acl nictool      path_beg /nictool
-    acl mediawiki    path_beg /wiki
-    acl mediawiki    path_beg /w/
-    acl smf          path_beg /forum
-    acl stage        path_beg /stage
+	acl munin        path_beg /munin
+	acl nagios       path_beg /nagios
+	acl watch        path_beg /watch
+	acl haraka       path_beg /haraka
+	acl haraka       path_beg /logs
+	acl qmailadmin   path_beg /qmailadmin
+	acl qmailadmin   path_beg /cgi-bin/qmailadmin
+	acl sqwebmail    path_beg /sqwebmail
+	acl sqwebmail    path_beg /cgi-bin/sqwebmail
+	acl isoqlog      path_beg /isoqlog
+	acl rspamd       path_beg /rspamd
+	acl roundcube    path_beg /roundcube
+	acl rainloop     path_beg /rainloop
+	acl squirrelmail path_beg /squirrelmail
+	acl nictool      path_beg /nictool
+	acl mediawiki    path_beg /wiki
+	acl mediawiki    path_beg /w/
+	acl smf          path_beg /forum
+	acl wordpress    path_beg /wordpress
+	acl stage        path_beg /stage
 
-    use_backend websocket_haraka if  is_websocket
-    use_backend www_monitor      if  munin
-    use_backend www_monitor      if  nagios
-    use_backend www_haraka       if  watch
-    use_backend www_vpopmail     if  qmailadmin
-    use_backend www_sqwebmail    if  sqwebmail
-    use_backend www_vpopmail     if  isoqlog
-    use_backend www_haraka       if  haraka
-    use_backend www_rspamd       if  rspamd
-    use_backend www_roundcube    if  roundcube
-    use_backend www_rainloop     if  rainloop
-    use_backend www_squirrelmail if  squirrelmail
-    use_backend www_nictool      if  nictool
-    use_backend www_mediawiki    if  mediawiki
-    use_backend www_smf          if  smf
-    use_backend www_stage        if  stage
+	use_backend websocket_haraka if  is_websocket
+	use_backend www_monitor      if  munin
+	use_backend www_monitor      if  nagios
+	use_backend www_haraka       if  watch
+	use_backend www_vpopmail     if  qmailadmin
+	use_backend www_sqwebmail    if  sqwebmail
+	use_backend www_vpopmail     if  isoqlog
+	use_backend www_haraka       if  haraka
+	use_backend www_rspamd       if  rspamd
+	use_backend www_roundcube    if  roundcube
+	use_backend www_rainloop     if  rainloop
+	use_backend www_squirrelmail if  squirrelmail
+	use_backend www_nictool      if  nictool
+	use_backend www_mediawiki    if  mediawiki
+	use_backend www_smf          if  smf
+	use_backend www_wordpress    if  wordpress
+	use_backend www_stage        if  stage
 
-    default_backend www_webmail
+	# for Let's Encrypt SSL/TLS certificates
+	use_backend www_webmail      if { path_beg -i /.well-known }
+
+	default_backend www_webmail
 
 backend www_vpopmail
-    server vpopmail $(get_jail_ip vpopmail):80
+	server vpopmail $(get_jail_ip vpopmail):80
 
 backend www_sqwebmail
-    server sqwebmail $(get_jail_ip sqwebmail):80
+	server sqwebmail $(get_jail_ip sqwebmail):80
 
 backend www_haraka
-    server haraka $(get_jail_ip haraka):80
-    reqirep ^([^\ :]*)\ /haraka/(.*)    \1\ /\2
+	server haraka $(get_jail_ip haraka):80
+	reqirep ^([^\ :]*)\ /haraka/(.*)    \1\ /\2
 
 backend websocket_haraka
-    timeout queue 5s
-    timeout server 86400s
-    timeout connect 86400s
-    server haraka $(get_jail_ip haraka):80
+	timeout queue 5s
+	timeout server 86400s
+	timeout connect 86400s
+	server haraka $(get_jail_ip haraka):80
 
 backend www_webmail
-    server webmail $(get_jail_ip webmail):80
+	server webmail $(get_jail_ip webmail):80
 
 backend www_roundcube
-    server roundcube $(get_jail_ip roundcube):80
-    reqirep ^([^\ :]*)\ /roundcube/(.*)    \1\ /\2
+	server roundcube $(get_jail_ip roundcube):80
+	reqirep ^([^\ :]*)\ /roundcube/(.*)    \1\ /\2
 
 backend www_squirrelmail
-    server squirrelmail $(get_jail_ip squirrelmail):80
+	server squirrelmail $(get_jail_ip squirrelmail):80
 
 backend www_rainloop
-    server rainloop $(get_jail_ip rainloop):80
-    reqirep ^([^\ :]*)\ /rainloop/(.*)    \1\ /\2
+	server rainloop $(get_jail_ip rainloop):80
+	reqirep ^([^\ :]*)\ /rainloop/(.*)    \1\ /\2
 
 backend www_monitor
-    server monitor $(get_jail_ip monitor):80
+	server monitor $(get_jail_ip monitor):80
 
 backend www_rspamd
-    server monitor $(get_jail_ip rspamd):11334
-    reqirep ^([^\ :]*)\ /rspamd/(.*)    \1\ /\2
+	server monitor $(get_jail_ip rspamd):11334
+	reqirep ^([^\ :]*)\ /rspamd/(.*)    \1\ /\2
 
 backend www_nictool
-    server monitor $(get_jail_ip nictool):80
-    reqirep ^([^\ :]*)\ /nictool/(.*)    \1\ /\2
+	server monitor $(get_jail_ip nictool):80
+	reqirep ^([^\ :]*)\ /nictool/(.*)    \1\ /\2
 
 backend www_mediawiki
-    server monitor $(get_jail_ip mediawiki):80
+	server monitor $(get_jail_ip mediawiki):80
 
 backend www_smf
-    server monitor $(get_jail_ip smf):80
+	server monitor $(get_jail_ip smf):80
 
 backend www_wordpress
-    server monitor $(get_jail_ip wordpress):80
+	server monitor $(get_jail_ip wordpress):80
 
 backend www_stage
-    server monitor $(get_jail_ip stage):80
+	server monitor $(get_jail_ip stage):80
 
 EO_HAPROXY_CONF
+
+	rm "$STAGE_MNT/usr/local/etc/haproxy.conf"
+	stage_exec ln -s /data/etc/haproxy.conf /usr/local/etc/haproxy.conf
 
 	if ls /etc/ssl/private/*.pem; then
 		tell_status "copying PEM files"
@@ -171,14 +183,20 @@ EO_HAPROXY_CONF
 			> "$STAGE_MNT/etc/ssl/private/server.pem" || exit 1
 	fi
 
-    if [ ! -d "$ZFS_DATA_MNT/haproxy/ssl" ]; then
-        mkdir -p "$ZFS_DATA_MNT/haproxy/ssl" || exit 1
-    fi
+	if [ ! -d "$ZFS_DATA_MNT/haproxy/ssl" ]; then
+		tell_status "creating /data/ssl"
+		mkdir -p "$ZFS_DATA_MNT/haproxy/ssl" || exit 1
+	fi
 
-    if [ ! -f "$ZFS_DATA_MNT/haproxy/ssl/dhparam.pem" ]; then
-        tell_status "creating dhparam file for haproxy"
-        openssl dhparam 2048 -out "$ZFS_DATA_MNT/haproxy/ssl/dhparam.pem"
-    fi
+	if [ ! -d "$ZFS_DATA_MNT/haproxy/ssl.d" ]; then
+		tell_status "creating /data/ssl.d"
+		mkdir -p "$ZFS_DATA_MNT/haproxy/ssl.d" || exit 1
+	fi
+
+	if [ ! -f "$ZFS_DATA_MNT/haproxy/ssl/dhparam.pem" ]; then
+		tell_status "creating dhparam file for haproxy"
+		openssl dhparam 2048 -out "$ZFS_DATA_MNT/haproxy/ssl/dhparam.pem"
+	fi
 }
 
 start_haproxy()
