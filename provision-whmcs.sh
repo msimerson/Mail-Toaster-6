0cd83785ad8bea4b35e1d4439533cfa80e240786
Matt Simerson <matt@tnpi.net>
2017-01-22 16:18:34 -0800

    whmcs: use include/nginx

--- provision-whmcs.sh
+++ provision-whmcs.sh
@@ -7,34 +7,12 @@ export JAIL_START_EXTRA=""
 export JAIL_CONF_EXTRA=""
 
 mt6-include php
-
-install_nginx()
-{
-	tell_status "installing nginx"
-	stage_pkg_install nginx
-
-	if [ ! -f "$ZFS_JAIL_MNT/whmcs/var/db/ports/www_nginx/options" ]; then
-		return
-	fi
-
-	if [ ! -d "$STAGE_MNT/var/db/ports/www_nginx" ]; then
-		tell_status "creating /var/db/ports/www_nginx" 
-		mkdir -p  "$STAGE_MNT/var/db/ports/www_nginx" || exit
-	fi
-
-	tell_status "copying www_nginx/options"
-	cp "$ZFS_JAIL_MNT/whmcs/var/db/ports/www_nginx/options" \
-		"$STAGE_MNT/var/db/ports/www_nginx/options" || exit
-
-	tell_status "installing nginx port with localized options"
-	stage_pkg_install GeoIP dialog4ports gettext
-	stage_exec make -C /usr/ports/www/nginx build deinstall install clean
-}
+mt6-include nginx
 
 install_whmcs()
 {
 	install_php 70 "ctype curl filter gd iconv imap json mbstring mcrypt openssl pdo_mysql session soap xml xmlrpc zip zlib"
-	install_nginx
+	install_nginx whmcs
 
 	stage_pkg_install sudo
 }
@@ -42,11 +20,7 @@ install_whmcs()
 configure_whmcs()
 {
 	configure_php
-	stage_sysrc nginx_enable=YES
-	stage_sysrc nginx_flags='-c /data/etc/nginx.conf'
-
-	mkdir -p "$STAGE_MNT/var/log/http"
-	chown www:www "$STAGE_MNT/var/log/http"
+	configure_nginx whmcs
 
 	mkdir -p "$STAGE_MNT/vendor/whmcs/whmcs"
 	chown -R www:www "$STAGE_MNT/vendor"
@@ -61,14 +35,12 @@ EO_CRONTAB
 start_whmcs()
 {
 	php_fpm_start
-	stage_exec service nginx start
+	start_nginx
 }
 
 test_whmcs()
 {
-	tell_status "testing httpd"
-	stage_listening 80
-
+	test_nginx
 	php_fpm_test
 	echo "it worked"
 }
