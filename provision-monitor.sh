1a93b6f8456412bf5b4eaf70a3d250e8366640c2
Matt Simerson <matt@tnpi.net>
2017-01-22 16:06:20 -0800

    monitor: installs munin, nrpe, & lighttpd
    for general purpose monitoring

--- provision-monitor.sh
+++ provision-monitor.sh
@@ -8,13 +8,64 @@ export JAIL_CONF_EXTRA=""
 
 install_monitor()
 {
-	tell_status "installing monitoring apps"
-	stage_pkg_install nagios nrpe swaks p5-Net-SSLeay || exit
+	tell_status "installing swaks"
+	stage_pkg_install swaks p5-Net-SSLeay || exit
+
+	tell_status "installing lighttpd"
+	stage_pkg_install lighttpd
+	stage_sysrc lighttpd_enable="YES"
+	stage_sysrc lighttpd_conf="/data/etc/lighttpd.conf"
+
+	install_nagios
+	install_munin
+}
+
+install_nagios()
+{
+	tell_status "installing nagios & nrpe"
+	stage_pkg_install nagios nrpe-ssl
+}
+
+install_munin()
+{
+	tell_status "installing munin"
+	stage_pkg_install munin-node munin-master
+}
+
+configure_munin()
+{
+	rm -r "$STAGE_MNT/usr/local/etc/munin"
+	stage_exec ln -s /data/etc/munin /usr/local/etc/munin
+
+	stage_sysrc munin_node_enable=YES
+	stage_sysrc munin_node_config=/data/etc/munin/munin-node.conf
+}
+
+configure_nrpe()
+{
+	if [ -f "$ZFS_DATA_MNT/monitor/etc/nrpe.cfg" ]; then
+		tell_status "preserving nrpe.cfg"
+		rm "$STAGE_MNT/usr/local/etc/nrpe.cfg"
+	else
+		tell_status "installing default nrpe.cfg"
+		mv "$STAGE_MNT/usr/local/etc/nrpe.cfg" \
+			"$ZFS_DATA_MNT/monitor/etc/nrpe.cfg"
+	fi
+
+	stage_exec ln -s /data/etc/nrpe.cfg /usr/local/etc/nrpe.cfg
+	stage_sysrc nrpe2_enable="YES"
+	stage_sysrc nrpe2_configfile=/data/etc/nrpe.cfg
 }
 
 configure_monitor()
 {
 	tell_status "configuring monitor"
+	if [ ! -d "$ZFS_DATA_MNT/monitor/etc" ]; then
+		mkdir "$ZFS_DATA_MNT/monitor/etc"
+	fi
+
+	configure_nrpe
+	configure_munin
 }
 
 start_monitor()
