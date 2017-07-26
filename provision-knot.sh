ada0a1d32b4883ef01c69e2561f6d9e0f88ab322
Matt Simerson <matt@tnpi.net>
2017-07-26 16:33:10 -0700

    add Knot DNS support
    WIP

---
+++ provision-knot.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# shellcheck disable=1091
+. mail-toaster.sh || exit
+
+export JAIL_START_EXTRA=""
+export JAIL_CONF_EXTRA=""
+
+install_knot()
+{
+	tell_status "installing Knot DNS 2"
+	stage_pkg_install knot2 rsync dialog4ports || exit
+
+	if [ ! -d "$STAGE_MNT/data/home" ]; then
+		mkdir -p "$STAGE_MNT/data/home" || exit
+	fi
+}
+
+configure_knot()
+{
+	stage_sysrc knot_enable="YES"
+}
+
+start_knot()
+{
+	tell_status "starting knot daemon"
+	stage_exec service knot start || exit
+}
+
+test_knot()
+{
+	tell_status "testing knot"
+	stage_test_running knot
+
+	stage_listening 53
+	echo "it worked."
+
+	tell_status "testing UDP DNS query"
+	drill    www.example.com @"$(get_jail_ip stage)" || exit
+
+	tell_status "testing TCP DNS query"
+	drill -t www.example.com @"$(get_jail_ip stage)" || exit
+}
+
+base_snapshot_exists || exit
+create_staged_fs knot
+start_staged_jail knot
+install_knot
+configure_knot
+configure_axfrdns
+start_knot
+test_knot
+promote_staged_jail knot
