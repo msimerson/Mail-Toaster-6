d2ddab9a5ec80cb2e963081fe9a49e15d501e459
Matt Simerson <matt@tnpi.net>
2017-11-04 22:06:45 -0700

    vpopmail: do not leave .bak file in periodic dir

--- provision-vpopmail.sh
+++ provision-vpopmail.sh
@@ -155,7 +155,7 @@ install_quota_report()
 	fetch -o "$_qr" "$TOASTER_SRC_URL/qmail/toaster-quota-report" || exit
 	chmod 755 "$_qr" || exit
 
-	sed -i .bak \
+	sed -i '' \
 		-e "/\$admin/ s/postmaster@example.com/$TOASTER_ADMIN_EMAIL/" \
 		-e "/assistance/ s/example.com/$TOASTER_HOSTNAME/" \
 		"$_qr"
