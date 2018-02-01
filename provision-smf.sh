8caa663a3975a0e859569c6a17b358445aa0c482
Matt Simerson <matt@tnpi.net>
2018-01-31 21:16:54 -0800

    smf: add PHP filter module (#325)

--- provision-smf.sh
+++ provision-smf.sh
@@ -12,7 +12,7 @@ mt6-include nginx
 install_smf()
 {
 	install_nginx
-	install_php 72 "mysqli session gd zlib"
+	install_php 72 "filter gd mysqli pdo_mysql session zlib"
 
 	if [ ! -d "$STAGE_MNT/usr/local/www/smf" ]; then
 		mkdir -p "$STAGE_MNT/usr/local/www/smf" || exit
