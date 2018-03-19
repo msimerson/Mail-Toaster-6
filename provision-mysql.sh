8a5ddbd498944d946e39d5e1aa97b02d877162ca
Rob Lensen <rob@bsdfreaks.nl>
2018-03-19 14:10:41 +0100

    Upgrade MariaDB to latest stable release 10.2

--- provision-mysql.sh
+++ provision-mysql.sh
@@ -26,7 +26,7 @@ install_mysql()
 install_mariadb()
 {
 	tell_status "installing mariadb"
-	stage_pkg_install mariadb101-server || exit
+	stage_pkg_install mariadb102-server || exit
 }
 
 configure_mysql()
