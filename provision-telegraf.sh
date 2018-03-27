a1332f36c534515bde5c8d6804481a3722d34fe8
Matt Simerson <matt@tnpi.net>
2018-03-27 12:43:18 -0700

    telegraf: configure data dir and url

--- provision-telegraf.sh
+++ provision-telegraf.sh
@@ -15,6 +15,15 @@ install_telegraf()
 	stage_sysrc telegraf_enable=YES
 }
 
+config_telegraf()
+{
+	local _conf="$STAGE_MNT/usr/local/etc/telegraf.conf"
+
+    sed -i.bak \
+        -e "s/urls.*8086/ s/127.0.0.1/172.16.15.50/"
+        "$_conf"
+}
+
 start_telegraf()
 {
 	stage_exec service telegraf start
@@ -29,6 +38,7 @@ base_snapshot_exists || exit
 create_staged_fs telegraf
 start_staged_jail telegraf
 install_telegraf
+config_telegraf
 start_telegraf
 test_telegraf
 promote_staged_jail telegraf
