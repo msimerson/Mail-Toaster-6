6f373cbc2b1760b8ae5e8fff40f10e6c8948e7ff
Matt Simerson <matt@tnpi.net>
2018-04-18 22:51:01 -0700

    update grafana to install v5 (#343)
    * update grafana to install v5

--- provision-grafana.sh
+++ provision-grafana.sh
@@ -8,10 +8,30 @@ export JAIL_CONF_EXTRA=""
 
 install_grafana()
 {
-	tell_status "installing Grafana"
-	stage_pkg_install grafana || exit
+	tell_status "installing Grafana 5"
+	stage_pkg_install grafana5 || exit
+}
 
-	tell_status "Enable Grafana"
+configure_grafana()
+{
+	if [ ! -d "$STAGE_MNT/data/etc" ]; then
+		mdkir "$STAGE_MNT/data/etc" || exit
+	fi
+
+	if [ ! -f "$STAGE_MNT/data/etc/grafana.conf" ]; then
+		tell_status "installing default grafana.conf"
+		cp "$STAGE_MNT/usr/local/etc/grafana.conf" "$STAGE_MNT/data/etc/grafana.conf" || exit
+	fi
+
+	stage_sysrc grafana_conf="/data/etc/grafana.conf"
+
+	if [ ! -d "$STAGE_MNT/data/db" ]; then
+		tell_status "creating grafana data/db dir"
+		mdkir "$STAGE_MNT/data/db" || exit
+		chown 904:904 "$STAGE_MNT/data/db"
+	fi
+
+	tell_status "Enabling Grafana"
 	stage_sysrc grafana_enable=YES
 }
 
@@ -29,6 +49,7 @@ base_snapshot_exists || exit
 create_staged_fs grafana
 start_staged_jail grafana
 install_grafana
+configure_grafana
 start_grafana
 test_grafana
 promote_staged_jail grafana
