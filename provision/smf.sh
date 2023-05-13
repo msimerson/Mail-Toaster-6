38d79e112e669911df41f7a33e5b1345020c3607
Matt Simerson <matt@tnpi.net>
2023-05-13 11:27:32 -0700

    smf: do more default config

--- provision/smf.sh
+++ provision/smf.sh
@@ -8,6 +8,8 @@ export JAIL_CONF_EXTRA=""
 mt6-include php
 mt6-include nginx
 
+SMF_VER=2.1.4
+
 install_smf()
 {
 	install_nginx
@@ -18,10 +20,10 @@ install_smf()
 	fi
 
 	fetch -m -o "$STAGE_MNT/data/smf.tar.gz" \
-		https://github.com/SimpleMachines/SMF/archive/refs/tags/v2.1.4.tar.gz || exit
+		https://github.com/SimpleMachines/SMF/archive/refs/tags/v$SMF_VER.tar.gz || exit
 
 	stage_exec sh -c 'cd /usr/local/www; tar -xzf /data/smf.tar.gz' || exit
-	stage_exec sh -c 'cd /usr/local/www && ln -s SMF-2.1.4 smf'
+	stage_exec sh -c "cd /usr/local/www && mv SMF-$SMF_VER smf" || exit
 
 	for _f in attachments avatars cache Packages Packages/installed.list Smileys Themes agreement.txt Settings.php Settings_bak.php; do
 		if [ -e "$STAGE_MNT/usr/local/www/smf/$_f" ]; then
@@ -37,14 +39,14 @@ configure_nginx_server()
 	 _NGINX_SERVER='
 		server_name         smf;
 
-		location /forum/images/custom_avatars/ {
+		location /smf/images/custom_avatars/ {
 			alias /data/custom_avatars/;
 			index index.php;
 			expires max;
 			try_files $uri =404;
 		}
 
-		location ~ ^/forum/(.+\.php)(/.*)?$ {
+		location ~ ^/smf/(.+\.php)(/.*)?$ {
 			alias          /usr/local/www/smf;
 			include        /usr/local/etc/nginx/fastcgi_params;
 			fastcgi_pass   php;
@@ -53,7 +55,7 @@ configure_nginx_server()
 			fastcgi_param  PATH_INFO $2;
 		}
 
-		location /forum/ {
+		location /smf/ {
 			alias /usr/local/www/smf/;
 			index index.php;
 			try_files $uri $uri/ =404;
@@ -69,12 +71,18 @@ configure_smf()
 	configure_nginx smf
 	configure_nginx_server
 
-	if [ -f "ZFS_DATA_MNT/smf/Settings.php" ]; then
-		tell_status "installing localized Settings.php"
-		cp "ZFS_DATA_MNT/smf/Settings.php" "$STAGE_MNT/usr/local/www/smf/" || exit
+	if [ -f "$ZFS_DATA_MNT/smf/Settings.php" ]; then
+		tell_status "preserving Settings.php"
+		cp "$ZFS_DATA_MNT/smf/Settings.php" "$STAGE_MNT/usr/local/www/smf/" || exit
 	else
+		tell_status "installing sample Settings.php"
+		cp "$STAGE_MNT/usr/local/www/smf/other/Settings.php" "$STAGE_MNT/usr/local/www/smf/" || exit
+
 		tell_status "post-install configuration will be required"
-		sleep 2
+		sed -i '' \
+			-e "/boardurl/ s/127.0.0.2/$TOASTER_HOSTNAME/" \
+			-e "/db_server/ s/localhost/mysql/" \
+			"$STAGE_MNT/usr/local/www/smf/Settings.php"
 	fi
 }
 
