6474f1251b9a3feb855552aa8fb27fdc7ba70742
Matt Simerson <matt@tnpi.net>
2023-05-28 15:34:29 -0700

    ubuntu: use case

--- provision/ubuntu.sh
+++ provision/ubuntu.sh
@@ -3,7 +3,7 @@
 . mail-toaster.sh || exit
 
 # tested with bionic (18), focal (20) and jammy (22)
-UBUNTU_RELEASE="jammy"
+DEBIAN_RELEASE="jammy"
 
 export JAIL_START_EXTRA="allow.mount
 		allow.mount.devfs
@@ -24,7 +24,7 @@ export JAIL_CONF_EXTRA='allow.raw_sockets;
 		mount += "/tmp      $path/compat/linux/tmp     nullfs    rw  0 0";
 		#mount += "/home     $path/compat/linux/home    nullfs    rw  0 0";'
 
-install_ubuntu()
+enable_linuxulator()
 {
 	tell_status "enabling Linux emulation on Host (loads kernel modules)"
 	sysrc linux_enable=YES
@@ -36,33 +36,46 @@ install_ubuntu()
 	stage_sysrc linux_mounts_enable=NO
 	stage_exec service linux start
 
-	tell_status "installing Ubuntu $UBUNTU_RELEASE"
+}
+
+install_ubuntu()
+{
+	enable_linuxulator
+
+	tell_status "installing $DEBIAN_RELEASE"
 	stage_pkg_install debootstrap || exit 1
-	stage_exec debootstrap $UBUNTU_RELEASE /compat/linux
+	stage_exec debootstrap $DEBIAN_RELEASE /compat/linux
 }
 
 configure_ubuntu()
 {
-	if [ -f "$STAGE_MNT/compat/linux/etc/apt/sources.list" ]; then
-		tell_status "restoring APT sources"
-		tee "$STAGE_MNT/compat/linux/etc/apt/sources.list" <<EO_SOURCES
-deb http://archive.ubuntu.com/ubuntu $UBUNTU_RELEASE main universe restricted multiverse
-deb http://security.ubuntu.com/ubuntu/ $UBUNTU_RELEASE-security universe multiverse restricted main
-deb http://archive.ubuntu.com/ubuntu $UBUNTU_RELEASE-backports universe multiverse restricted main
-deb http://archive.ubuntu.com/ubuntu $UBUNTU_RELEASE-updates universe multiverse restricted main
+	case "$DEBIAN_RELEASE" in
+		bionic)
+		focal)
+		jammy)
+			if [ -f "$STAGE_MNT/compat/linux/etc/apt/sources.list" ]; then
+				tell_status "restoring APT sources"
+				tee "$STAGE_MNT/compat/linux/etc/apt/sources.list" <<EO_SOURCES
+deb http://archive.ubuntu.com/ubuntu $DEBIAN_RELEASE main universe restricted multiverse
+deb http://security.ubuntu.com/ubuntu/ $DEBIAN_RELEASE-security universe multiverse restricted main
+deb http://archive.ubuntu.com/ubuntu $DEBIAN_RELEASE-backports universe multiverse restricted main
+deb http://archive.ubuntu.com/ubuntu $DEBIAN_RELEASE-updates universe multiverse restricted main
 EO_SOURCES
-	fi
+			fi
+			;;
+	esac
 }
 
 start_ubuntu()
 {
-	if [ "$UBUNTU_RELEASE" = "bionic" ]; then
-		stage_exec chroot /compat/linux apt remove -y rsyslog
-	fi
-
-	if [ "$UBUNTU_RELEASE" = "jammy" ]; then
-		stage_exec mount -t devfs devfs /compat/linux/dev
-	fi
+	case "$DEBIAN_RELEASE" in
+		bionic)
+			stage_exec chroot /compat/linux apt remove -y rsyslog
+			;;
+		jammy)
+			stage_exec mount -t devfs devfs /compat/linux/dev
+			;;
+	esac
 
 	tell_status "updating apt"
 	stage_exec chroot /compat/linux apt update || exit 1
@@ -70,9 +83,11 @@ start_ubuntu()
 	tell_status "updating installed apt packages"
 	stage_exec chroot /compat/linux apt upgrade -y || exit 1
 
-	if [ "$UBUNTU_RELEASE" = "jammy" ]; then
-		stage_exec umount /compat/linux/dev
-	fi
+	case "$DEBIAN_RELEASE" in
+		jammy)
+			stage_exec umount /compat/linux/dev
+			;;
+	esac
 }
 
 test_ubuntu()
