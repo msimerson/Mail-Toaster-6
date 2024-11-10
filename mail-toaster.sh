552dbed693e7aaf950bf8c6da4444518669805e4
Matt Simerson <matt@tnpi.net>
2024-11-09 21:34:25 -0800

    mt: update pfrule.sh

--- mail-toaster.sh
+++ mail-toaster.sh
@@ -183,7 +183,7 @@ export TOASTER_EDITOR_PORT=${TOASTER_EDITOR_PORT:="vim-tiny"}
 # See https://github.com/msimerson/Mail-Toaster-6/wiki/MySQL
 export TOASTER_MYSQL=${TOASTER_MYSQL:="1"}
 export TOASTER_MARIADB=${TOASTER_MARIADB:="0"}
-export TOASTER_NTP=${TOASTER_NTP:="ntp"}
+export TOASTER_NTP=${TOASTER_NTP:="openntpd"}
 export TOASTER_MSA=${TOASTER_MSA:="haraka"}
 export TOASTER_PKG_AUDIT=${TOASTER_PKG_AUDIT:="0"}
 export TOASTER_PKG_BRANCH=${TOASTER_PKG_BRANCH:="latest"}
@@ -589,35 +589,69 @@ install_pfrule()
 
 # pfrule.sh
 #
-# Matt Simerson, matt@tnpi.net, 2023-06
+# Matt Simerson, matt@tnpi.net, 2024-11
 #
 # Use pfctl to load and unload PF rules into named anchors from config
 # files. See https://github.com/msimerson/Mail-Toaster-6/wiki/PF
 
-_etcpath="$(dirname -- "$( readlink -f -- "$0"; )";)"
+ETC_PATH="$(dirname -- "$( readlink -f -- "$0"; )";)"
+JAIL_NAME=$(basename "$(dirname "$(dirname $ETC_PATH)")")
+PREVIEW="$2"
 
 usage() {
-    echo "   usage: $0 [ load | unload ]"
+    echo "   usage: $0 [ load | unload ] [-n]"
     echo " "
     exit 1
 }
 
-for _f in "$_etcpath"/*.conf; do
+load_tables() {
+    for _f in "$ETC_PATH"/*.table; do
+        [ -f "$_f" ] || continue
+        _table_name=$(basename $_f .table)
+        do_cmd "pfctl -t "$_table_name" -T replace -f "$_f""
+    done
+}
+
+flush_tables() {
+    for _f in "$ETC_PATH"/*.table; do
+        [ -f "$_f" ] || continue
+        _table_name=$(basename $_f .table)
+        do_cmd "pfctl -t "$_table_name" -T flush"
+    done
+}
+
+do_cmd() {
+    if [ "$PREVIEW" = "-n" ]; then
+        echo "$1"
+    else
+        $1 || exit 1
+    fi
+}
+
+flush() {
+    case "$1" in
+        "nat"   ) do_cmd "$2 -F nat"   ;;
+        "filter") do_cmd "$2 -F rules" ;;
+    esac
+}
+
+# load tables first, they may be referenced in anchored files
+if [ "$1" = "load" ]; then load_tables; fi
+
+for _anchor in binat nat rdr filter; do
+    _f="$ETC_PATH/$_anchor.conf"
     [ -f "$_f" ] || continue
 
-    _anchor=$(basename $_f .conf)  # nat, rdr, allow
-    _jailname=$(basename "$(dirname "$(dirname $_etcpath)")")
-    _pfctl="pfctl -a $_anchor/$_jailname"
+    _pfctl="pfctl -a $_anchor/$JAIL_NAME"
 
     case "$1" in
-        "load"   ) _cmd="$_pfctl -f $_f" ;;
-        "unload" ) _cmd="$_pfctl -F all" ;;
+        "load"   ) do_cmd "$_pfctl -f $_f" ;;
+        "unload" ) flush "$_anchor" "$_pfctl" ;;
         *        ) usage                 ;;
     esac
-
-    echo "$_cmd"
-    $_cmd || exit 1
 done
+
+if [ "$1" = "unload" ]; then flush_tables; fi
 
 exit
 EO_PF_RULE
