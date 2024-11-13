7c9448da984e0b238f97e55860e8ee5d1366940d
Matt Simerson <matt@tnpi.net>
2024-11-12 18:13:52 -0800

    fix

--- mail-toaster.sh
+++ mail-toaster.sh
@@ -354,7 +354,7 @@ base_snapshot_exists()
 jail_conf_header()
 {
 	local _path="$ZFS_JAIL_MNT/$1"
-	if [ "$1" = "base" ]; then _path="$BASE_MNT" fi
+	if [ "$1" = "base" ]; then _path="$BASE_MNT"; fi
 
 	cat <<EO_JAIL_CONF_HEAD
 exec.start = "/bin/sh /etc/rc";
