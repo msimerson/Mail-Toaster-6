ca90fe3b37de82e01c6ca2c4cc5ec424b6cf7571
Phil Krylov <phil.krylov@gmail.com>
2025-03-21 12:48:06 +0100

    [hardening] mt: mount data volume nosuid

--- mail-toaster.sh
+++ mail-toaster.sh
@@ -199,7 +199,7 @@ install_fstab()
 
 	tell_status "writing data mount to $_fstab"
 	echo "# Device                Mountpoint      FStype  Options         Dump    Pass#" | tee "$_fstab" || exit 1
-	echo "$_data_mount       $_jail_mount/data nullfs  rw   0  0" | tee -a "$_fstab"
+	echo "$_data_mount       $_jail_mount/data nullfs  rw,nosuid   0  0" | tee -a "$_fstab"
 
 	if [ -n "$JAIL_FSTAB" ]; then
 		tell_status "appending JAIL_FSTAB to fstab"
