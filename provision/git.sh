813ddbc5ccaaf36b11281141bb6a66973a924b23
Matt Simerson <matt@tnpi.net>
2026-06-26 16:44:34 -0700

    git: enabled, expanded

--- provision/git.sh
+++ provision/git.sh
@@ -8,9 +8,7 @@ export JAIL_START_EXTRA=""
 export JAIL_CONF_EXTRA=""
 export JAIL_FSTAB=""
 
-preflight_check() {
-	echo ""
-}
+mt6-include nginx
 
 install_git()
 {
@@ -20,27 +18,120 @@ install_git()
 	done
 
 	tell_status "install git"
-	stage_pkg_install git
+	stage_pkg_install git cgit nginx fcgiwrap
+}
+
+configure_nginx_server()
+{
+	_NGINX_SERVER="
+	server_name git default_server;
+	root /data/htdocs;
+
+	location /.well-known/acme-challenge {
+		try_files \$uri =404;
+	}
+
+	location /.well-known/pki-validation {
+		try_files \$uri =404;
+	}
+
+	# Forbid access to other dotfiles
+	location ~ /\.(?!well-known).* {
+		return 403;
+	}
+
+    client_max_body_size 100M;
+
+    location ~ /[^/]+\.git/(HEAD|info/refs|objects/info/[^/]+|git-(upload|receive)-pack)\$ {
+        root   /data/repos;
+
+        include fastcgi_params;
+
+        fastcgi_param SCRIPT_FILENAME /usr/local/libexec/git-core/git-http-backend;
+        fastcgi_param GIT_HTTP_EXPORT_ALL "";
+        fastcgi_param GIT_PROJECT_ROOT /data/repos;
+        fastcgi_param PATH_INFO \$uri;
+
+        fastcgi_param REMOTE_USER \$remote_user;
+
+        fastcgi_pass unix:/var/run/fcgiwrap/fcgiwrap.sock;
+    }
+
+    location / {
+        root   /data/repos;
+        try_files      \$uri @cgit;
+    }
+
+    location @cgit {
+        include        fastcgi_params;
+        # Route to the cgit CGI executable instead of Git's backend
+        fastcgi_param  SCRIPT_FILENAME   /usr/local/www/cgit/cgit.cgi;
+        fastcgi_param  PATH_INFO         \$uri;
+        fastcgi_param  QUERY_STRING      \$args;
+        fastcgi_param  HTTP_HOST         \$server_name;
+        fastcgi_pass   unix:/var/run/fcgiwrap/fcgiwrap.sock;
+    }
+	"
+	export _NGINX_SERVER
+	configure_nginx_server_d git
+}
+
+configure_pf()
+{
+	_pf_etc="$ZFS_DATA_MNT/git/etc/pf.conf.d"
+
+	store_config "$_pf_etc/rdr.conf" <<EO_GIT_RDR
+int_ip4 = "$(get_jail_ip git)"
+int_ip6 = "$(get_jail_ip6 git)"
+
+rdr inet  proto tcp from any to <ext_ip4> port { 8080 } -> \$int_ip4 80
+rdr inet6 proto tcp from any to <ext_ip6> port { 8080 } -> \$int_ip6 80
+EO_GIT_RDR
+	fi
+
+	get_public_ip4
+	get_public_ip6
+
+	store_config "$_pf_etc/git.table" <<EO_WEBMAIL_TABLE
+$PUBLIC_IP4
+$PUBLIC_IP6
+$(get_jail_ip git)
+$(get_jail_ip6 git)
+EO_WEBMAIL_TABLE
+
+	store_config "$_pf_etc/filter.conf" <<EO_WEBMAIL_FILTER
+pass in quick proto tcp from any to <git> port { 80 8080 }
+EO_WEBMAIL_FILTER
 }
 
 configure_git()
 {
+	configure_nginx_server
+	configure_pf
+
 	stage_sysrc sshd_enable=YES
+	stage_sysrc fcgiwrap_enable=YES
+	stage_sysrc fcgiwrap_user="www"
+	stage_sysrc fcgiwrap_group="www"
+	stage_sysrc fcgiwrap_socket_owner="www"
+	stage_sysrc fcgiwrap_socket_group="www"
+	stage_sysrc nginx_enable=YES
 }
 
 start_git()
 {
 	stage_exec service sshd start
+	stage_exec service fcgiwrap start
+	stage_exec service nginx start
 }
 
 test_git()
 {
 	echo "testing git..."
-	stage_exec ls /data/home/
+	stage_listening 80
 	echo "it worked"
 }
 
-preflight_check
 base_snapshot_exists || exit 1
 create_staged_fs git
 start_staged_jail git
