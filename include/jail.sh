01178ea537d21947f6df34f0b228055cdaa2aff5
Matt Simerson <matt@tnpi.net>
2026-08-03 13:29:43 -0700

    Apply suggestion from @msimerson

--- include/jail.sh
+++ include/jail.sh
@@ -116,7 +116,7 @@ EO_PF_INSECURE
 
 	store_config "$_pf_etc/rdr.conf" "overwrite" <<EO_PF_RDR
 int_ip6 = "$(get_jail_ip6 "$_jail")"
-rdr inet  proto tcp from any to <ext_ip4> port { $_ports } -> \$int_ip4
+rdr inet  proto tcp from any to <ext_ip4> port { $_ports } -> $(get_jail_ip "$_jail")
 rdr inet6 proto tcp from any to <ext_ip6> port { $_ports } -> $(get_jail_ip6 "$_jail")
 EO_PF_RDR
 
