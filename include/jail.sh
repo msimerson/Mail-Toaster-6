d2a406c985a08ade831ce05aff68a6ba3560b805
Matt Simerson <matt@tnpi.net>
2026-08-10 11:20:23 -0700

    consistent spacing

--- include/jail.sh
+++ include/jail.sh
@@ -165,7 +165,7 @@ jail_conf_header()
 exec.start = "/bin/sh /etc/rc";
 exec.stop = "/bin/sh /etc/rc.shutdown";
 exec.clean;
-devfs_ruleset=4;
+devfs_ruleset = 4;
 path = "$_path";
 interface = $JAIL_NET_INTERFACE;
 host.hostname = \$name;
