d5b2ee53931794093d4c3eba9e1b5ae2fd20cf1d
Phil Krylov <phil@krylov.eu>
2026-08-12 12:36:45 +0200

    Add dns to the minimal /etc/hosts
    Co-authored-by: Matt Simerson <matt@tnpi.net>

--- include/network.sh
+++ include/network.sh
@@ -110,6 +110,7 @@ EO_ACME_CRON
 install_minimal_hosts()
 {
 	store_config "$STAGE_MNT/etc/hosts" "append" <<EO_HOSTS
+$(get_jail_ip dns) dns
 $(get_jail_ip syslog) syslog
 $(get_jail_ip bsd_cache) pkg vulnxml freebsd-update
 EO_HOSTS
